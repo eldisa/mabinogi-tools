806286ed0ab03a2112816d520605cf596746c543
eldisa <maplestory54007@gmail.com>
2025-04-28 00:15:10 +0800

    fix: import error

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import About from '../views/About.vue';
 
 import PierecingLevelAnalysis from '../views/PierecingLevelAnalysis.vue';
